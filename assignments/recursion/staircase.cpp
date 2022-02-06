2d59483b354f7faa7f720260fd28865393ea9384
jubuyer <jahmed10@stuy.edu>
2022-02-05 22:57:10 -0500

    implemented memoization to staircase problem

--- assignments/recursion/staircase.cpp
+++ assignments/recursion/staircase.cpp
@@ -13,11 +13,11 @@ int stepPerms(int n) {
 	// }
 
 	//-------- After understanding base cases---------------
-	//base case, when there is one step, there is one way 
+	//base case, when there is one step, there is one way
 	//to climb the stair case
 	if(n == 1) {
 		return 1;
-	} else if (n == 2) { 
+	} else if (n == 2) {
 		return 1 + stepPerms(n-1);
 	} else if (n == 3) {
 		return 2 + stepPerms(n-1);
@@ -27,13 +27,42 @@ int stepPerms(int n) {
 	}
 }
 
+int combo[100000];
+int memo_stepPerms(int n) {
+	if (combo[n] != 0) {
+		return combo[n];
+	} else if (n < 2) {
+		combo[n] = 1;
+	} else if (n == 2) {
+		combo[n] = 2;
+	} else if (n == 3) {
+		combo[n] = 4;
+	} else {
+		combo[n] = memo_stepPerms(n-3) + memo_stepPerms(n-2) + memo_stepPerms(n-1);
+	}
+
+	return combo[n];
+}
+
 int main(int argc, char const *argv[])
 {
-	std::cout << "height: 1 -> " << stepPerms(1) << "\n";
-	std::cout << "height: 2 -> " << stepPerms(2) << "\n";
-	std::cout << "height: 3 -> " << stepPerms(3) << "\n";
-	std::cout << "height: 5 -> " << stepPerms(5) << "\n";
-	std::cout << "height: 7 -> " << stepPerms(7) << "\n";
-	std::cout << "height: 21 -> " << stepPerms(21) << "\n";
+	// std::cout << "height: 1 -> " << stepPerms(1) << "\n"; // 1
+	// std::cout << "height: 2 -> " << stepPerms(2) << "\n"; // 2
+	// std::cout << "height: 3 -> " << stepPerms(3) << "\n"; // 4
+	// std::cout << "height: 5 -> " << stepPerms(5) << "\n"; // 13
+	// std::cout << "height: 7 -> " << stepPerms(7) << "\n"; //44
+	// std::cout << "height: 21 -> " << stepPerms(21) << "\n"; //still ok 223317
+	// std::cout << "height: 31 -> " << stepPerms(31) << "\n"; //begins to slow
+	// std::cout << "height: 41 -> " << stepPerms(41) << "\n"; //slow
+
+	std::cout << "height: 1 -> " << memo_stepPerms(1) << "\n";
+	std::cout << "height: 2 -> " << memo_stepPerms(2) << "\n";
+	std::cout << "height: 3 -> " << memo_stepPerms(3) << "\n";
+	std::cout << "height: 5 -> " << memo_stepPerms(5) << "\n";
+	std::cout << "height: 7 -> " << memo_stepPerms(7) << "\n";
+	std::cout << "height: 21 -> " << memo_stepPerms(21) << "\n"; //still ok
+	std::cout << "height: 31 -> " << memo_stepPerms(31) << "\n"; //begins to slow
+	std::cout << "height: 41 -> " << memo_stepPerms(41) << "\n"; //slow
+
 	return 0;
 }
