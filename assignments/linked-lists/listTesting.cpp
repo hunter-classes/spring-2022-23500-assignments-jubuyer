d27cc9d5ab68069ad696c6b34fe4994fa22fb6b1
jubuyer <jahmed10@stuy.edu>
2022-03-02 09:57:58 -0500

    old main for List.cpp

---
+++ assignments/linked-lists/listTesting.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "List.h"
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Linked Lists" << '\n';
+
+  std::cout << "Test List:" << '\n';
+  List *l1 = new List();
+  l1->insert("a");
+  l1->insert("b");
+  l1->insert("c");
+
+  std::cout << l1->toString() << "\n\n";
+
+  std::cout << "Testing locate()" << '\n';
+  l1->locate(3,"x");
+  std::cout << l1->toString() << "\n\n";
+
+  std::cout << "Testing remove()" << '\n';
+  l1->remove(7);
+  std::cout << l1->toString() << "\n\n";
+
+  std::cout << "Testing destructor" << '\n';
+  delete l1;
+  //std::cout << l1->toString() << '\n';
+
+  return 0;
+}
