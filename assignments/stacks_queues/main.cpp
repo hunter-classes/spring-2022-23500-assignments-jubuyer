99316b0b6add2c52f396648b2112e9cdd20fda27
jubuyer <jubayer7a@gmail.com>
2022-04-19 14:25:48 -0400

    tests tests more tests

--- assignments/stacks_queues/main.cpp
+++ assignments/stacks_queues/main.cpp
@@ -4,6 +4,30 @@
 
 int main(int argc, char const *argv[])
 {
-	std::cout << "Hi\n"; 
-	return 0;
+	std::cout << "Stacks" << '\n';
+
+	std::cout << "Test Stack:" << '\n';
+	Stack *s1 = new Stack();
+	s1->push("a");
+	s1->push("b");
+	s1->push("c");
+
+	std::cout << s1->toString() << "\n\n";
+
+	s1->pop();
+	s1->pop();
+
+	std::cout << s1->toString() << "\n\n";
+
+	s1->pop();
+
+	std::cout << s1->toString() << "\n\n";
+
+	try {
+		s1->pop();
+	} catch (int e) {
+		std::cout << "ERR " << e << ": Empty stack\n";
+	}
+
+
 }
